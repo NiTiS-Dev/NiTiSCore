ef2c0eb0b4ecd7adf5c447393ba2aa4befaefb01
NickName73 <egorooz73@gmail.com>
2022-03-12 20:51:23 +0300

    update LICENSE and buildCore.cmd

--- buildCore.cmd
+++ buildCore.cmd
@@ -8,5 +8,7 @@ dotnet pack NiTiS.IO/ -c Release -o ../.build/
 dotnet pack NiTiS.Collections/ -c Release -o ../.build/
 dotnet pack NiTiS.Interaction/ -c Release -o ../.build/
 dotnet pack NiTiS.Math/ -c Release -o ../.build/
+dotnet pack NiTiS.Reflection/ -c Release -o ../.build/
+dotnet pack NiTiS.Additions/ -c Release -o ../.build/
 
 cd ..
