32a115b32beea0e37acf63d7834a3a3f2bbd6039
NickName73 <egorooz73@gmail.com>
2022-04-09 23:08:18 +0300

    New Enumerable Extensions

---
+++ Core/NiTiS.Collections/Enumerable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiTiS.Collections;
+
+public static class Enumerable
+{
+	public static IEnumerable<T> AppendRange<T>(this IEnumerable<T> @this, IEnumerable<T> range)
+	{
+		foreach(T i in @this)
+		{
+			yield return i;
+		}
+		foreach(T i in range)
+		{
+			yield return i;
+		}
+	}
+}
