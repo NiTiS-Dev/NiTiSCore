b81e410ef2a0cb2fe1d7afd215bc4b4382015174
NickName73 <egorooz73@gmail.com>
2022-04-10 00:37:29 +0300

    Add Digger

--- Core/NiTiS.Additions/Functions.cs
+++ Core/NiTiS.Additions/Functions.cs
@@ -1,5 +1,8 @@
 ﻿namespace NiTiS.Additions;
 
-public static class Functions
-{
-}
+public delegate T Dipper<T>(T item);
+
+public delegate bool Validator<in T>(T item);
+public delegate bool Validator<in T1, in T2>(T1 item1, T2 item2);
+public delegate bool Validator<in T1, in T2, in T3>(T1 item1, T2 item2, T3 item3);
+public delegate bool Validator<in T1, in T2, in T3, in T4>(T1 item1, T2 item2, T3 item3, T4 item4);
---
+++ Core/NiTiS.Collections/Pseudo/Iterator.cs
@@ -0,0 +1,22 @@
+﻿namespace NiTiS.Collections.Pseudo;
+
+public static class Iterator
+{
+	public static void Range(Iterate iterator, int start, int end)
+	{
+		while (start <= end)
+		{
+			iterator(start);
+			start++;
+		}
+	}
+	public static void Range(Iterate iterator, int start, uint amount)
+	{
+		for (uint i = 0; i < amount; i++)
+		{
+			iterator(start);
+			start++;
+		}
+	}
+}
+public delegate void Iterate(int index);
