4cba23fa71e07e3450a1cbdec67653ba6f7c360a
NickName73 <egorooz73@gmail.com>
2022-05-09 18:17:43 +0300

    a

--- Core/NiTiS.Math/VectorMath.cs
+++ Core/NiTiS.Math/VectorMath.cs
@@ -5,40 +5,15 @@ namespace NiTiS.Math;
 
 public static class VectorMath
 {
-	public static Vector4D Vector4DOf(float size)
-	{
-		return new Vector4D(size, size, size, size);
-	}
-	public static Vector3D Vector3DOf(float size)
-	{
-		return new Vector3D(size, size, size);
-	}
-	public static Vector2D Vector2DOf(float size)
-	{
-		return new Vector2D(size, size);
-	}
-	public static Vector1D Vector1DOf(float size)
-	{
-		return new Vector1D(size);
-	}
-	public static float Normalize(float value)
+	internal static float Normalize(float value)
 	{
 		if (value == 0f) return 0f;
 		if (value < 0f) return -1f; else return 1f; 
 	}
-	public static int Normalize(int value)
+	internal static int Normalize(int value)
 	{
 		if (value == 0) return 0;
 		if (value < 0) return -1; else return 1;
-	}
-	public static bool CompareVectors<T>(IVector<T> left, IVector<T> right, EnumCollection<Axis> axis)
-	{
-		foreach (Axis i in axis)
-		{
-			if (!left.GetValueByDimension(i)!.Equals(right.GetValueByDimension(i)))
-				return false;
-		}
-		return true;
 	}
 	public static bool CompareVectors<T>(IVector<T> left, IVector<T> right, params Axis[] axis)
 	{
